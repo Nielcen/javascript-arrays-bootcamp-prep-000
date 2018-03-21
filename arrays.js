dc01a9863c2b8594a589c08d22a0ac90248a8771
James Ryan Nielcen <rnielcen@hotmail.com>
2018-03-21 16:15:46 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,29 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+ 
+ function addElementToBeginningOfArray(chocolateBars, foo){
+return ['foo', ...chocolateBars]
+ }
+ 
+ function destructivelyAddElementToBeginningOfArray(chocolateBars, foo){
+ [chocolateBars.unshift("foo")]
+ return chocolateBars
+}
+
+function addElementToEndOfArray(chocolateBars, foo){
+  return [...chocolateBars, 'foo']
+}
+
+function destructivelyAddElementToEndOfArray(chocolateBars, foo){
+  [chocolateBars.push('foo')]
+  return chocolateBars
+}
+
+function accessElementInArray(chocolateBars, index){
+return chocolateBars[2]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
+[chocolateBars.shift()]
+return chocolateBars
+}
+
